b95dddaf37569624c575fb1fd607a20feee692d3
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 01:06:26 +0300

    Update Package.swift

--- Tweak/Package.swift
+++ Tweak/Package.swift
@@ -26,7 +26,7 @@ let package: Package = .init(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Paisseon/Jinx.git", branch: "master")
+        .package(url: "https://github.com/Paisseon/Jinx.git", branch: "emt")
     ],
     targets: [
         .target(
