2d916ab003b67a398f46f3cc19bc9fe8c21d476f
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 16:27:56 +0300

    Update SatellaView.swift

--- Tweak/Sources/SatellaJailed/Views/SatellaView.swift
+++ Tweak/Sources/SatellaJailed/Views/SatellaView.swift
@@ -14,20 +14,23 @@ struct SatellaShapeView: UIViewRepresentable {
 
 @available(iOS 15, *)
 struct SatellaView: View {
-    @StateObject var model: SatellaModel = .shared
+    @ObservedObject var model: SatellaModel = .shared
     
     var body: some View {
-        Button {
-            model.isShowing.toggle()
-        } label: {
-            Color(red: 0.80, green: 0.63, blue: 0.87)
-                .mask {
-                    SatellaShapeView()
+        ZStack {
+            if model.isShowing {
+                PreferencesView(isShowing: $model.isShowing)
+            } else {
+                Button {
+                    model.isShowing.toggle()
+                } label: {
+                    Color(red: 0.80, green: 0.63, blue: 0.87)
+                        .mask {
+                            SatellaShapeView()
+                        }
                 }
-        }
-        .frame(width: 33, height: 33)
-        .sheet(isPresented: $model.isShowing) {
-            PreferencesView(isShowing: $model.isShowing)
+                .frame(width: 33, height: 33)
+            }
         }
     }
 }
