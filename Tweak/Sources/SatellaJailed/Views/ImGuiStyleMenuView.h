4d028598f1d4df5c1a6e9ee53925bbd91fec7063
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 16:47:50 +0300

    Create ImGuiStyleMenuView.h

---
+++ Tweak/Sources/SatellaJailed/Views/ImGuiStyleMenuView.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImGuiStyleMenuView : UIView
+@property (nonatomic, strong) UIView *menuWindow;
+@property (nonatomic, strong) UIButton *floatingIcon;
+@property (nonatomic, strong) UISlider *speedSlider;
+@end
+
+NS_ASSUME_NONNULL_END
