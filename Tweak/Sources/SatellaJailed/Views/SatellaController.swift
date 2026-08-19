ec3d976933cac7b4fa0521f53a83e4b4f1a07d87
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 17:23:30 +0300

    Update SatellaController.swift

--- Tweak/Sources/SatellaJailed/Views/SatellaController.swift
+++ Tweak/Sources/SatellaJailed/Views/SatellaController.swift
@@ -1,16 +1,15 @@
+import SwiftUI
 import UIKit
 
 @available(iOS 15, *)
 final class SatellaController: UIViewController {
     static let shared: SatellaController = .init()
     
-    var menuView: ImGuiStyleMenuView!
-    
-    // WindowHook entegrasyonu için pencere dizisi
+    var model: SatellaModel = .shared
+    var host: UIView = .init()
     var windows: [UIWindow] = []
     
     override func loadView() {
-        // PassthroughView yerine doğrudan bu tam ekran şeffaf menü yöneticisini kullanıyoruz
         view = PassthroughView()
     }
     
@@ -18,12 +17,33 @@ final class SatellaController: UIViewController {
         super.viewDidLoad()
         
         let size: CGSize = UIScreen.main.bounds.size
-        view.frame = CGRect(origin: .zero, size: size)
-        view.backgroundColor = .clear
+        let geoX: Double = size.width * 0.85
+        let geoY: Double = size.height * 0.5
         
-        // Objective-C menüsünü ekrana ekle
-        menuView = ImGuiStyleMenuView(frame: view.bounds)
-        menuView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        view.addSubview(menuView)
+        view.frame = CGRect(origin: .zero, size: size)
+        
+        let tellaVC: UIHostingController = .init(rootView: SatellaView())
+        let tapGest: UITapGestureRecognizer = .init(target: self, action: #selector(handleTap))
+        let panGest: UIPanGestureRecognizer = .init(target: self, action: #selector(handlePan))
+        
+        host = UIView(frame: CGRect(x: geoX, y: geoY, width: 33, height: 33))
+        
+        tellaVC.view.frame = CGRect(origin: .zero, size: CGSize(width: 33, height: 33))
+        tellaVC.view.backgroundColor = .clear
+        host.backgroundColor = .clear
+        
+        host.addGestureRecognizer(tapGest)
+        host.addGestureRecognizer(panGest)
+        
+        host.addSubview(tellaVC.view)
+        view.addSubview(host)
+    }
+    
+    @objc func handleTap() {
+        model.isShowing.toggle()
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        host.center = gesture.location(in: view)
     }
 }
