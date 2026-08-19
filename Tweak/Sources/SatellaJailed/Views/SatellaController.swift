f53cd0b94bb747495ceaf03a5ab06f1335908875
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 16:47:01 +0300

    Update SatellaController.swift

--- Tweak/Sources/SatellaJailed/Views/SatellaController.swift
+++ Tweak/Sources/SatellaJailed/Views/SatellaController.swift
@@ -1,15 +1,13 @@
-import SwiftUI
 import UIKit
 
 @available(iOS 15, *)
 final class SatellaController: UIViewController {
     static let shared: SatellaController = .init()
     
-    var model: SatellaModel = .shared
-    var host: UIView = .init()
-    var windows: [UIWindow] = []
+    var menuView: ImGuiStyleMenuView!
     
     override func loadView() {
+        // PassthroughView yerine doğrudan bu tam ekran şeffaf menü yöneticisini kullanıyoruz
         view = PassthroughView()
     }
     
@@ -17,34 +15,12 @@ final class SatellaController: UIViewController {
         super.viewDidLoad()
         
         let size: CGSize = UIScreen.main.bounds.size
-        let geoX: Double = size.width * 0.85
-        let geoY: Double = size.height * 0.5
-        
         view.frame = CGRect(origin: .zero, size: size)
+        view.backgroundColor = .clear
         
-        let tellaVC: UIHostingController = .init(rootView: SatellaView())
-        let tapGest: UITapGestureRecognizer = .init(target: self, action: #selector(handleTap))
-        let panGest: UIPanGestureRecognizer = .init(target: self, action: #selector(handlePan))
-        
-        host = UIView(frame: CGRect(x: geoX, y: geoY, width: 33, height: 33))
-        
-        tellaVC.view.frame = CGRect(origin: .zero, size: CGSize(width: 33, height: 33))
-        tellaVC.view.backgroundColor = .clear
-        host.backgroundColor = .clear
-        
-        host.addGestureRecognizer(tapGest)
-        host.addGestureRecognizer(panGest)
-        
-        host.addSubview(tellaVC.view)
-        view.addSubview(host)
-    }
-    
-    @objc func handleTap() {
-        model.isShowing.toggle()
-    }
-    
-    @objc func handlePan(gesture: UIPanGestureRecognizer) {
-        host.center = gesture.location(in: view)
+        // Objective-C menüsünü ekrana ekle
+        menuView = ImGuiStyleMenuView(frame: view.bounds)
+        menuView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(menuView)
     }
 }
-
